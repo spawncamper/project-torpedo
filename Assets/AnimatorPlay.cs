b400df2ad0c588883eb7399af206aa2ee3c07f9e
Mitch <ddiarikov@gmail.com>
2021-07-11 18:34:58 +0300

    trying to set up animator on Asteroids

---
+++ Assets/AnimatorPlay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class AnimatorPlay : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private string animator_state;
+    [SerializeField] private int animator_layer;
+    [SerializeField] private float normalized_time;
+
+    public void AnimatorPlayMethod()
+    {
+        animator.Play(animator_state, animator_layer, normalized_time);
+    }
+}
