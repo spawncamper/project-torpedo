13a75fb80d6f3e1de6a7ea57df636b6b2f7031ea
Mitch <ddiarikov@gmail.com>
2021-07-11 18:11:28 +0300

    added a reset trigger script

---
+++ Assets/ResetTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ResetTrigger : MonoBehaviour
+{
+    [SerializeField] Animator m_Animator;
+    [SerializeField] string animatorTrigger;
+
+    public void ResetAnimatorTrigger()
+    {
+        m_Animator.ResetTrigger(animatorTrigger);
+    }
+}
